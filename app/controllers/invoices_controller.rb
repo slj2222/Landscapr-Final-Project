568abb47271b7ab04cdecb4c8d78f3f6672ce51b
Spencer Johnson <sljohnson2222@gmail.com>
2022-05-07 14:20:09 -0500

    invoice patch working when marked as paid, still not updating state for the invoice list without refresh

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -5,10 +5,19 @@ class InvoicesController < ApplicationController
         render json: newInvoice
     end
 
+    def update
+        updateInvoice = Invoice.find_by(id: params[:id])
+        render json: updateInvoice.update(updateInvoiceParams)
+    end
+
     private
 
     def newInvoiceParams
         params.permit(:invoice_date, :invoice_amount, :client_id, :property_id, :collected, :invoiced)
+    end
+
+    def updateInvoiceParams
+        params.permit(:collected)
     end
 
 end
--- client/src/components/InvoiceSimpleCard.js
+++ client/src/components/InvoiceSimpleCard.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function InvoiceSimpleCard({ invoice }) {
+    const [isPaid, setIsPaid] = useState(false)
+
+
+
+    function handleClick() {
+
+        fetch(`/invoices/${invoice.id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({collected: true})
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+        setIsPaid(true)
+    }
+
+
     return (
         <div className="invoice-simple-card">
             <div>{invoice.invoice_date}</div>
             <div>property id: {invoice.property_id} </div>
             <div>invoice amount: ${invoice.invoice_amount}</div>
-            <div>
-                {invoice.collected ? <div> PAID </div> : 
-                    <div>
-                        <div> NOT PAID </div> 
-                        <button>mark as paid</button>
-                    </div>}
-            </div>
-                
+            
+            {isPaid ? <div> paid </div> :
+                <div>
+                    {invoice.collected ? <div> PAID </div> : 
+                        <div>
+                            <div> NOT PAID </div> 
+                            <button onClick={handleClick}>mark as paid</button>
+                        </div>}
+                </div>}
+                    
         </div>
     )
 }
